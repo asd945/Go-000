762172908ee568c2f27af08915e267dc330d5107
asdcu <402442701@qq.com>
2020-12-12 20:18:18 +0800

    fixed

--- Week03/main.go
+++ Week03/main.go
@@ -3,34 +3,32 @@ package main
 import (
 	"context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	group := errgroup.Group{}
-
+	baseCtx, cancel  := context.WithCancel(context.Background())
+	group, ctx := errgroup.WithContext(baseCtx)
 	server := &http.Server{Addr: ":8080", Handler: &Foo{}}
 	debug := &http.Server{Addr: ":8081", Handler: &Foo{}}
 
-	shouldStop := make(chan error, 2)
-
 	group.Go(func() (err error) {
-		defer func() {
-			shouldStop <- fmt.Errorf("server err, %v", err)
+		go func() {
+			<- ctx.Done()
+			_ = server.Shutdown(context.Background())
 		}()
 		fmt.Println("start http server...")
 		err = server.ListenAndServe()
 		return
 	})
 	group.Go(func() (err error) {
-		defer func() {
-			shouldStop <- fmt.Errorf("debug server err, %v", err)
+		go func() {
+			<- ctx.Done()
+			_ = debug.Shutdown(context.Background())
 		}()
 		fmt.Println("start debug server...")
 		err = debug.ListenAndServe()
@@ -40,21 +38,15 @@ func main() {
 	go func() {
 		signalChan := make(chan os.Signal, 1)
 		signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
-
 		select {
-		case err := <-shouldStop:
-			fmt.Println("receive err:", err)
 		case c := <-signalChan:
 			fmt.Println("receive signal: ", c)
+			cancel()
 		}
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		_ = server.Shutdown(ctx)
-		_ = debug.Shutdown(ctx)
 	}()
 
 	if err := group.Wait(); err != nil {
-		fmt.Println("all shutdown")
+		fmt.Println("all shutdown, err is:", err)
 	}
 
 }
